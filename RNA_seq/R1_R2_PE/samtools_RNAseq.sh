ddcdfa4214e93504417873636c48051aa6662a75
BDUAN <duanbbing@gmail.com>
2019-06-02 17:40:57 -0400

    Correction of samtools_RNAseq.sh

--- RNA_seq/R1_R2_PE/samtools_RNAseq.sh
+++ RNA_seq/R1_R2_PE/samtools_RNAseq.sh
@@ -14,9 +14,6 @@ for file in *hs2.sam; do echo $file && samtools view -S -b ${file} > ${file/sam/
 for file in *bam; do echo $file && samtools view -H ${file} > ${file/.bam/_header.sam}; done
 
 
-for f1 in *bam; do f2=${f1/.bam/_header.sam} && echo $f1 $f2 && samtools view ${f1} | grep -w "NH:i:1" | cat ${f2} - | samtools view -Sb - > ${f1/.bam/_unique.bam}; done
 
-
-for file in *unique.bam; do echo $file && samtools sort $file -o ${file/.bam/_sorted.bam} && rm $file; done
 
  
